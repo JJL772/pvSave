3ad7ea2e15709b1a341b8ab5ce40dc68c95e4c93
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2024-08-15 22:37:25 -0700

    initial

---
+++ iocBoot/ioc-pvSave-test/st.cmd
@@ -0,0 +1,21 @@
+#!../../bin/linux-x86_64/pvSaveTest
+
+#- You may have to change pvSaveTest to something else
+#- everywhere it appears in this file
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/pvSaveTest.dbd"
+pvSaveTest_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+#dbLoadRecords("db/pvSaveTest.db","user=jeremy")
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=jeremy"
